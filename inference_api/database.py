06d5390c89645efd20cb49ff12301574788c868d
Rishitha <sannapureddyrishitha@gmail.com>
2026-02-27 01:33:24 +0530

    Make database optional for cloud deployment

--- inference_api/database.py
+++ inference_api/database.py
@@ -4,15 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# --------------------------------------------------
-# Make DB Optional (Cloud Safe)
-# --------------------------------------------------
+engine = None
+SessionLocal = None
 
 if DATABASE_URL:
     engine = create_engine(DATABASE_URL)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-else:
-    engine = None
-    SessionLocal = None
 
 Base = declarative_base()
